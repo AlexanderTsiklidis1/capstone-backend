7bc90de692e06dee8b591699fa9d0c24ff330c66
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-02-19 21:04:09 -0500

    begins backend

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS capstone_interviews_dev;
+CREATE DATABASE capstone_interviews_dev;
+
+\c capstone_interviews_dev;
+
---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./app.js");
+
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
