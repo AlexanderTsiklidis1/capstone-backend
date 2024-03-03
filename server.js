96b3e7949b63b0d8aba05347da59b79ac8acaa6c
Anthony Huarneck <anthonyhuarneck@Anthonys-MacBook-Air.local>
2024-03-03 12:07:33 -0500

    Edits server.js; implements server for video functionality.

--- server.js
+++ server.js
@@ -1,10 +1,88 @@
-const app = require("./app.js");
-
-
 require("dotenv").config();
-const PORT = process.env.PORT;
 
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { default: fetch } = require("node-fetch");
+const jwt = require("jsonwebtoken");
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+const PORT = 9000;
+const app = express();
+const API_KEY = process.env.VIDEOSDK_API_KEY;
+const SECRET_KEY = process.env.VIDEOSDK_SECRET_KEY;
+
+console.log(API_KEY, SECRET_KEY)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+//
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+//
+app.get("/get-token", (req, res) => {
+
+  console.log("Token acquired")
+
+  const options = { expiresIn: "1000m", algorithm: "HS256" };
+
+  const payload = {
+    apikey: API_KEY,
+    permissions: ["allow_join", "allow_mod"], // also accepts "ask_join"
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, options);
+  res.json({ token });
+});
+
+//
+app.post("/create", (req, res) => {
+  const { token, region } = req.body;
+  const url = `${process.env.VIDEOSDK_API_ENDPOINT}/api/meetings`;
+  const options = {
+    method: "POST",
+    headers: { Authorization: token, "Content-Type": "application/json" },
+    body: JSON.stringify({ region }),
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((result) => res.json(result)) // result will contain meetingId
+    .catch((error) => console.error("error", error));
+});
+
+//
+app.post("/validate-meeting/:meetingId", (req, res) => {
+  const token = req.body.token;
+  const meetingId = req.params.meetingId;
+
+  const url = `${process.env.VIDEOSDK_API_ENDPOINT}/api/meetings/${meetingId}`;
+
+  const options = {
+    method: "POST",
+    headers: { Authorization: token },
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((result) => res.json(result)) // result will contain meetingId
+    .catch((error) => console.error("error", error));
+});
+
+//
+app.listen(PORT, () => {
+  console.log(`API server listening at http://localhost:${PORT}`);
+});
+
+
+
+// require("dotenv").config();
+// const PORT = process.env.PORT;
+
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}`);
+// });
