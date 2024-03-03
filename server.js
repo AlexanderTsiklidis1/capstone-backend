cc350a888e60850178ef5988aaabcf7479b1e2ab
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-03-03 12:54:13 -0500

    changes server.jus fordynamic import

--- server.js
+++ server.js
@@ -3,15 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { default: fetch } = require("node-fetch");
-const jwt = require("jsonwebtoken");
 
 const PORT = 9000;
 const app = express();
-const API_KEY = process.env.VIDEOSDK_API_KEY;
-const SECRET_KEY = process.env.VIDEOSDK_SECRET_KEY;
 
-console.log(API_KEY, SECRET_KEY)
+console.log(process.env.VIDEOSDK_API_KEY, process.env.VIDEOSDK_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,20 +23,23 @@ app.get("/get-token", (req, res) => {
 
   console.log("Token acquired")
 
+  const jwt = require("jsonwebtoken");
+
   const options = { expiresIn: "1000m", algorithm: "HS256" };
 
   const payload = {
-    apikey: API_KEY,
+    apikey: process.env.VIDEOSDK_API_KEY,
     permissions: ["allow_join", "allow_mod"], // also accepts "ask_join"
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, options);
+  const token = jwt.sign(payload, process.env.VIDEOSDK_SECRET_KEY, options);
   res.json({ token });
 });
 
 //
 app.post("/create", (req, res) => {
   const { token, region } = req.body;
+  const fetch = require("node-fetch");
   const url = `${process.env.VIDEOSDK_API_ENDPOINT}/api/meetings`;
   const options = {
     method: "POST",
@@ -59,6 +58,7 @@ app.post("/validate-meeting/:meetingId", (req, res) => {
   const token = req.body.token;
   const meetingId = req.params.meetingId;
 
+  const fetch = require("node-fetch");
   const url = `${process.env.VIDEOSDK_API_ENDPOINT}/api/meetings/${meetingId}`;
 
   const options = {
@@ -76,13 +76,3 @@ app.post("/validate-meeting/:meetingId", (req, res) => {
 app.listen(PORT, () => {
   console.log(`API server listening at http://localhost:${PORT}`);
 });
-
-
-
-// require("dotenv").config();
-// const PORT = process.env.PORT;
-
-
-// app.listen(PORT, () => {
-//   console.log(`Listening on port ${PORT}`);
-// });
