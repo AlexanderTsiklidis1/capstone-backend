9c75f4b00ec33733b7d19aca3d37573f4261c5dd
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-03-10 14:14:56 -0400

    Complete 'getReviewByInterview' query.

--- queries/interviewReviews.js
+++ queries/interviewReviews.js
@@ -1,1 +1,13 @@
 const db = require('../db/dbConfig');
+
+const getReviewByInterview = async (interviewId) => {
+	try {
+		const reviews = await db.one(
+			'SELECT * FROM reviews WHERE interviews.id = $1',
+			interviewId
+		);
+		return reviews;
+	} catch (error) {
+		console.error(error);
+	}
+};
