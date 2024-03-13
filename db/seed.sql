9ddc11896a8f09e7582e46957c8992834f9fa2cd
Anthony Huarneck <anthonyhuarneck@Anthonys-MacBook-Air.local>
2024-03-12 21:10:36 -0400

    edits prompts table in schema. Completes seed for prompts table.

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,20 @@
 \c capstone_interviews_dev;
+
+INSERT INTO prompts (interview_type, category, prompt)
+VALUES
+('Behavioral', 'Projects', 'How did you decide to develop this app?'),
+('Behavioral', 'Projets', 'What tech are you using and how did you decide to use it?'),
+('Behavioral', 'Teamwork', 'Have you navigated a difficult situation with a coworker? What steps did you take to solve the problem?'),
+('Behavioral', 'Teamwork', 'Tell me about a situation where you had to deal with a very frustrated customer or client.'),
+('Behavioral', 'Teamwork', 'How do you handle conflict?'),
+('Behavioral', 'Growth', 'What is something you wish to grow in and how do you plan to improve?'),
+('Behavioral', 'Growth', 'What strengths do you bring to the table?'),
+('Behavioral', 'Growth', 'What are your greatest weaknesses?'),
+('Behavioral', 'Growth', 'Tell me about your proudest accomplishments?'),
+('Behavioral', 'Problem Solving', 'Tell me about a time when you faced a problem you could not solve. What did you do?'),
+('Behavioral', 'Problem Solving', 'Give me an example of a time when you did not know how to solve a problem and you used your resources to find the answer.'),
+('Behavioral', 'Problem Solving', 'What do you do when you have a lot of competing responsibilities? Do you have a plan for dealing with stress?'),
+('Behavioral', 'Problem Solving', 'Tell me about a time when you made a mistake or missed a deadline. What did you do to reorganize yourself in order to complete the project?'),
+('Behavioral', 'Leadership', 'Tell me about a time that you took the lead on a difficult project.'),
+('Behavioral', 'Leadership', 'Tell me about a time when you had to go above and beyond the call of duty in order to get a job done. What did that process look like?'),
+('Behavioral', 'Leadership', 'How would you go about motivating a team of people?')
