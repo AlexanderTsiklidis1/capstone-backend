792587441cd298db1987edc5fc644f193532784d
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-03-17 17:13:51 -0400

    Create 'getAllBookingsByUser', 'getOneBookingByUser'.

---
+++ controllers/bookingsController.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bookings = express.Router({ mergeParams: true });
+const {
+	getAllBookingsByUser,
+	getOneBookingByUser,
+	createBooking,
+	deleteBooking,
+	updateBooking,
+} = require('../queries/bookings');
+
+const { getOneUser } = require('../queries/users');
+
+bookings.get('/', async (req, res) => {
+	const { userId } = req.params;
+	try {
+		const bookingsByUser = await getAllBookingsByUser(userId);
+		res.json(bookingsByUser);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+bookings.get('/:bookingId', async (req, res) => {
+	const { userId, bookingId } = req.params;
+	try {
+		const user = await getOneUser(userId);
+		const bookingByUser = await getOneBookingByUser(userId, bookingId);
+		if (user && bookingByUser) {
+			res.json(bookingByUser);
+		} else {
+			res.status(404).json({ message: 'User or Booking not found.' });
+		}
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+module.exports = bookings;
